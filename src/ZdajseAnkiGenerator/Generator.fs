6736ecbbbf0f75a0f91575624ee087648300a2a9
Daniel Olczyk <contact@dolczyk.rocks>
2025-01-13 23:41:37 +0100

    Add remember selected checkbox to back of the card

--- src/ZdajseAnkiGenerator/Generator.fs
+++ src/ZdajseAnkiGenerator/Generator.fs
@@ -3,9 +3,10 @@ module Generator
 open System.IO
 open AnkiNet
 open FSharp.Json
+open Resource
 open Types
 
-let getChecked (checkedField : bool) = if checkedField then "checked" else ""
+let getCorrect (checkedField : bool) = if checkedField then "correct" else ""
 
 let getParsedSchema (path : string) = task {
     let! json = File.ReadAllTextAsync path
@@ -17,10 +18,12 @@ let sanitizeValue (value : string) = value.Replace(@"""", @"\""").Replace("'", @
 
 let generateDeck (jsonPath : string, outputPath : string) = async {
     let! schema = getParsedSchema jsonPath |> Async.AwaitTask
-    let cardType = AnkiCardType("Basic", 0, "{{Front}}", "{{Front}}<hr id=\"answer\">{{Back}}")
+    let frontTemplate = getResourceFile "front.html"
+    let backTemplate = getResourceFile "back.html"
+    let cardType = AnkiCardType("Quiz", 0, frontTemplate, backTemplate) 
     
     let noteType = AnkiNoteType(
-        "Basic",
+        "Quiz",
         [| cardType |],
         [| "Front"; "Back" |]
     )
@@ -30,9 +33,9 @@ let generateDeck (jsonPath : string, outputPath : string) = async {
     let deckId = collection.CreateDeck(schema.id)
     
     for data in schema.data do
-        let allAnswers = data.answers |> Array.map (fun x -> $"<input type='checkbox'><label>{x.answer}</label><br>") |> String.concat ""
-        let correctAnswers = data.answers |> Array.map (fun x -> $"<input type='checkbox' {getChecked x.correct}><label>{x.answer}</label><br>") |> String.concat ""
-        collection.CreateNote(deckId, noteTypeId, sanitizeValue(data.question + "<br>" + allAnswers), sanitizeValue(correctAnswers))
+        let answers = data.answers |> Array.mapi (fun i x -> $"<input id='{i}' type='checkbox'><label class='{getCorrect x.correct}'>{x.answer}</label><br>") |> String.concat ""
+        let sanitizedCard = sanitizeValue(data.question + "<br>" + answers)
+        collection.CreateNote(deckId, noteTypeId, sanitizedCard, sanitizedCard)
    
     let! writeResult = AnkiFileWriter.WriteToFileAsync($"{outputPath}/{schema.id}.apkg", collection) |> Async.AwaitTask
     writeResult
--- src/ZdajseAnkiGenerator/Program.fs
+++ src/ZdajseAnkiGenerator/Program.fs
@@ -6,7 +6,7 @@ open Arguments
 [<EntryPoint>]
 let main argv =
     let parser =
-        ArgumentParser.Create<CliArguments>(programName = "Vulder.SchoolScrapper")
+        ArgumentParser.Create<CliArguments>(programName = "ZdajseAnkiGenerator")
         
     match parser.ParseCommandLine argv with
     | p when p.Contains(Output_Path) && p.Contains(Json_File) -> Generator.generateDeck(p.GetResult(Json_File), p.GetResult(Output_Path)) |> Async.RunSynchronously
